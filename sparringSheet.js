29f03f53a88af0e18cab73683c78f49a643cb47d
alanjwade <51059522+alanjwade@users.noreply.github.com>
2022-07-17 17:40:16 -0600

    Bracket and 3rd place print out.

--- sparringSheet.js
+++ sparringSheet.js
@@ -10,6 +10,7 @@ function generateSparringSheet(sourceSheetName = "Beginner") {
     var [peopleArr, virtToPhysMap] = readTableIntoArr(sourceSheet)
   
     var targetSheet = SpreadsheetApp.getActive().getSheetByName(targetSheetName)
+    targetSheet.clear()
 
     var physToVirtMap = physToVirtMapInv(virtToPhysMap)
   
@@ -24,7 +25,9 @@ function generateSparringSheet(sourceSheetName = "Beginner") {
         .sort(sortBySparringOrder)
 
         // Generate one bracket
-        generateOneSparringBracket(targetSheet, virtRingPeople, 1, 1)
+        generateOneSparringBracket(targetSheet, virtRingPeople, 0, 0, 5)
+        // Generate the 3rd place bracket
+        generateOneSparringBracket(targetSheet, virtRingPeople, 29, 3, 2)
   
       // Now, virtRingPeople has all the people in one virt ring AND is doing forms
       var body = targetDoc.getBody()
@@ -77,35 +80,83 @@ function getCoordinatesFromRoundPosition(round, position) {
                                              // round 4: offset 7
                                              // round 5: offset 15
 
-  spacing = round * 2 // round 1: spacing = 2
-                      // round 2: spacing = 4 etc
+  spacing = Math.pow(2, round) // round 1: spacing = 2
+                               // round 2: spacing = 4
+                               // round 3: spacing = 8
 
   thisRow = startRowOffset + (position * spacing)
 
   return [thisRow, col]
 }
+
+function placePeopleInBracket(peopleArr, startRow=0, startCol=0, rounds = 5) {
+  var totalPeople = peopleArr.length
+
+  // Find which round we're going to start in
+
+  var startRound = 0
+  var start2Round = 0
+
+  var numStartRound = 0
+  var numStart2Round = 0
+
+  for (var round = 1; round <= rounds; round++) {
+    var numThisRound = Math.pow(2, rounds-round)
+    var numNextRound = Math.pow(2, rounds - (round + 1))
+    if (totalPeople == numThisRound) {
+      startRound = round
+      start2Round = round
+      numStartRound = totalPeople
+      numStart2Round = 0
+      break
+    }
+    else if ((totalPeople < numThisRound) & (totalPeople > numNextRound)) {
+      startRound = round
+      start2Round = round + 1
+
+      // need an even number to fight in the start round
+      numStartRound = 2 * Math.round(totalPeople - numNextRound / 2);
+      numStart2Round = totalPeople = numStartRound
+      break
+    }
+  }
+
+  // we put numStartRound in round 'startRound' and numStart2Round in 'start2Round'
+
+  var position = 0
+
+  //for (var r1 = )
+
+}
   
-function generateOneSparringBracket(targetSheet, peopleArr, startRow, startCol) {
+function generateOneSparringBracket(targetSheet, peopleArr, startRow = 0, startCol = 0, rounds = 5) {
     // Generate a single sparring bracket
 
     var totalPeople = peopleArr.length
     // Determine the number of rounds
 
     //var rounds = Math.ceil(Math.log2(totalPeople))
-    var rounds = 5
 
     var curCol = 0;
 
-    for (var round=1; round<rounds; round++) {
+    for (var round=1; round<=rounds; round++) {
       var maxPositions = Math.pow(2, rounds - round)
 
+      var lastRow = 0
       for (var position=0; position < maxPositions; position++) {
         [row, col] = getCoordinatesFromRoundPosition(round, position)
 
-        targetSheet.getRange(row+1, col+1).setBorder(null, null, true, null, null, null)
+        targetSheet.getRange(startRow + row + 1, startCol + col + 1).setBorder(null, null, true, null, null, null)
+
+        // Make the right borders
+        // If we're an odd position, make a right border between this and the last position
+        if (position % 2 != 0) {
+          for (var borderRows = lastRow+1; borderRows <= row; borderRows++) {
+            targetSheet.getRange(startRow + borderRows + 1, startCol + col + 1).setBorder(null, null, null, true, null, null)
+          }
+        }
+
+        lastRow = row
       }
-
-
     }
-
 }
